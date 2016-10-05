6e77819b957ede6b89a8e70b9d0deea45bb7fcb1
phuti <phuti>
2016-10-05 13:37:44 +0200

    1st Commit of the from my quizz app

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.android.quizzapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import static com.example.android.quizzapp.R.layout.activity_main;
+import static com.example.android.quizzapp.R.layout.activity_questions;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    RadioButton rdb_selected;
+    RadioGroup radioM;
+
+    //Global Variables
+    ViewFlipper VF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+
+        /*
+         * Find the views declared in main.xml.
+         */
+        radioM = (RadioGroup) findViewById(R.id.rdgMain);
+        radioM.setOnCheckedChangeListener(this);
+
+    }
+
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        rdb_selected = (RadioButton) findViewById(i);
+
+        String heading = rdb_selected.getText().toString();
+
+        Intent intent = new Intent(this, Questions.class);
+        intent.putExtra("Head", heading);
+        startActivity(intent);
     }
 }
+
+
+
+
+
+
+
+
+
+
